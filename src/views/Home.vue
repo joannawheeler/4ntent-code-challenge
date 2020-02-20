e31e3acf6b0071dc75fc760804608d547077fee0
Mamunur Rashid <rimonmath@gmail.com>
2020-02-20 12:07:38 +0600

    everything implemented successfully

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,39 +9,7 @@
     </div>
 
     <v-container class="my-plan-container" v-else>
-      <h1 class="text-center mb-2">WELLNESS PLAN</h1>
-
-      <v-avatar color="indigo">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <span>
-        NAME
-      </span>
-
-      <v-avatar color="indigo" class="ml-3">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <span>
-        MENTOR NAME
-      </span>
-
-      <v-card class="my-plan-card mt-4">
-        <v-card-text>
-          <p><strong>MY INTENSION: </strong> My intension</p>
-          <p><strong>WHERE AM I NOW: </strong> Where am i now</p>
-          <p><strong>WHERE DO I WANT TO GO: </strong> Where do i want to go</p>
-          <p><strong>WHAT DO I CARE ABOUT: </strong> What do I care about</p>
-        </v-card-text>
-      </v-card>
-
-      <v-card class="my-plan-card mt-5">
-        <v-card-text>
-          <p><strong>PRO-ACTIVE STRATEGIES: </strong> Pro-active stratigies</p>
-          <p><strong>REWARD STRATEGIES: </strong> Reward strategies</p>
-          <p><strong>MAINTENENCE: </strong> Maintenence</p>
-          <p><strong>SUPPORTS: </strong> Supports</p>
-        </v-card-text>
-      </v-card>
+      <my-plan :plan="myPlan"></my-plan>
       <div class="text-center mt-5 mb-5">
         <v-btn color="primary" @click="printModal = true"
           >DOWNLOAD MY PLAN</v-btn
@@ -50,44 +18,7 @@
     </v-container>
 
     <my-print-modal v-model="printModal" @print="printNow">
-      <h1 class="text-center">WELLNESS PLAN</h1>
-
-      <v-avatar color="indigo">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <span>
-        NAME
-      </span>
-
-      <v-avatar color="indigo" class="ml-3">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <span>
-        MENTOR NAME
-      </span>
-
-      <br />
-      <br />
-
-      <v-card class="my-plan-card">
-        <v-card-text>
-          <p><strong>MY NTENSION: </strong> My ntension</p>
-          <p><strong>WHERE AM I NOW: </strong> Where am i now</p>
-          <p><strong>WHERE DO I WANT TO GO: </strong> Where do i want to go</p>
-          <p><strong>WHAT DO I CARE ABOUT: </strong> What do I care about</p>
-        </v-card-text>
-      </v-card>
-
-      <br /><br />
-
-      <v-card class="my-plan-card">
-        <v-card-text>
-          <p><strong>PRO-ACTIVE STRATEGIES: </strong> Pro-active stratigies</p>
-          <p><strong>REWARD STRATEGIES: </strong> Reward strategies</p>
-          <p><strong>MAINTENENCE: </strong> Maintenence</p>
-          <p><strong>SUPPORTS: </strong> Supports</p>
-        </v-card-text>
-      </v-card>
+      <my-plan :plan="myPlan"></my-plan>
     </my-print-modal>
   </div>
 </template>
